25bf7d86963005495b3174d9532a5683f4204b9f
Yegor Trussov <egortr2004@mail.ru>
2020-04-02 21:32:27 +0600

    todolist with TS finished

--- vanilla-poject/new/app.js
+++ vanilla-poject/new/app.js
@@ -1,6 +1,7 @@
 var Todo = /** @class */ (function () {
-    function Todo(name, description, completed) {
+    function Todo(name, id, description, completed) {
         this.name = name;
+        this.id = id;
         this.description = description;
         this.completed = completed;
     }
@@ -10,8 +11,8 @@ var TodoList = /** @class */ (function () {
     function TodoList() {
         this.allTodos = new Array;
     }
-    TodoList.prototype.createTodoItem = function (name, description) {
-        var newItem = new Todo(name, description, false);
+    TodoList.prototype.createTodoItem = function (name, id, description) {
+        var newItem = new Todo(name, id, description, false);
         var totalCount = this.allTodos.push(newItem);
         return totalCount;
     };
@@ -19,21 +20,46 @@ var TodoList = /** @class */ (function () {
         return this.allTodos;
     };
     TodoList.prototype.deleteItem = function (id) {
-        this.allTodos.splice(id, 1);
+        var todos = this.allTodos;
+        todos = todos.filter(function (todo) { return todo.id !== id; });
+        this.allTodos = todos;
+        showAllTodos();
         return this.allTodos;
     };
     return TodoList;
 }());
 var todoList = new TodoList;
+var container = document.getElementById('todo-list');
 window.onload = function () {
     var name = document.getElementById("todoName");
     var description = document.getElementById("todoDescription");
     document.getElementById("add").addEventListener('click', function () {
-        todoList.createTodoItem(name.value, description.value);
-        console.log(todoList.allTodoItems());
-    });
-    document.getElementById('delete').addEventListener('click', function () {
-        var id = document.getElementById('deleteTodo');
-        console.log(todoList.deleteItem(id));
+        var id = Math.random().toString();
+        todoList.createTodoItem(name.value, id, description.value);
+        container.appendChild(createTodoElement(name.value, id, description.value));
     });
 };
+function createTodoElement(name, id, description) {
+    var div = document.createElement('div');
+    var nameSpan = document.createElement('span');
+    var descriptionSpan = document.createElement('span');
+    var deleteBtn = document.createElement('button');
+    deleteBtn.setAttribute('data-id', id);
+    deleteBtn.addEventListener('click', function () {
+        todoList.deleteItem(id);
+    });
+    nameSpan.innerText = name;
+    descriptionSpan.innerText = description;
+    div.appendChild(nameSpan);
+    div.appendChild(document.createElement('br'));
+    div.appendChild(descriptionSpan);
+    div.appendChild(deleteBtn);
+    div.appendChild(document.createElement('hr'));
+    return div;
+}
+function showAllTodos() {
+    container.innerHTML = '';
+    todoList.allTodos.forEach(function (todo) {
+        container.append(createTodoElement(todo.name, todo.id, todo.description));
+    });
+}
--- vanilla-poject/new/app.ts
+++ vanilla-poject/new/app.ts
@@ -2,15 +2,18 @@ interface ITodo {
     name: string;
     description: string;
     completed: boolean;
+    id: string;
 }
 
 class Todo implements ITodo {
     public name: string;
+    public id: string;
     public description: string;
     public completed: boolean;
 
-    constructor (name: string, description: string, completed: boolean) {
+    constructor (name: string, id: string, description: string, completed: boolean) {
         this.name = name;
+        this.id = id;
         this.description = description;
         this.completed = completed;
     }
@@ -19,8 +22,8 @@ class Todo implements ITodo {
 class TodoList {
     public allTodos: Todo[] = new Array;
 
-    createTodoItem(name: string, description: string): number {
-        let newItem = new Todo(name, description, false);
+    createTodoItem(name: string, id: string, description: string): number {
+        let newItem = new Todo(name, id, description, false);
         let totalCount: number = this.allTodos.push(newItem);
         return totalCount;
     }
@@ -29,13 +32,18 @@ class TodoList {
         return this.allTodos;
     }
 
-    deleteItem(id): Todo[] {
-        this.allTodos.splice(id, 1);
+    deleteItem(id: string): Todo[] {
+        let todos: Todo[] = this.allTodos;
+        todos = todos.filter(todo => todo.id !== id);
+        this.allTodos = todos;
+        showAllTodos();
         return this.allTodos;
     }
 }
 
 let todoList = new TodoList;
+
+let container = document.getElementById('todo-list');
 
 window.onload = function(){
     let name= <HTMLInputElement>document.getElementById("todoName");
@@ -43,12 +51,36 @@ window.onload = function(){
                        "todoDescription");
     document.getElementById("add").addEventListener(
       'click', () => {
-        todoList.createTodoItem(name.value, description.value);
-        console.log(todoList.allTodoItems());
+        let id: string = Math.random().toString();
+        todoList.createTodoItem(name.value, id, description.value);
+        container.appendChild(createTodoElement(name.value, id, description.value));
       }); 
+}
+
+function createTodoElement(name: string, id: string, description: string): HTMLDivElement {
+    let div = <HTMLDivElement> document.createElement('div');
+    let nameSpan = <HTMLSpanElement> document.createElement('span');
+    let descriptionSpan = <HTMLSpanElement> document.createElement('span');
+    let deleteBtn = <HTMLButtonElement> document.createElement('button');
     
-    document.getElementById('delete').addEventListener('click', () => {
-        let id = <HTMLInputElement> document.getElementById('deleteTodo');
-        console.log(todoList.deleteItem(id));
+    deleteBtn.setAttribute('data-id', id);
+    deleteBtn.addEventListener('click', () => {
+        todoList.deleteItem(id);
+    })
+    nameSpan.innerText = name;
+    descriptionSpan.innerText = description;
+    div.appendChild(nameSpan);
+    div.appendChild(document.createElement('br'));
+    div.appendChild(descriptionSpan);
+    div.appendChild(deleteBtn);
+    div.appendChild(document.createElement('hr'));
+
+    return div;
+} 
+
+function showAllTodos(): void {
+    container.innerHTML = '';
+    todoList.allTodos.forEach(todo => {
+        container.append(createTodoElement(todo.name, todo.id, todo.description));
     })
 }
