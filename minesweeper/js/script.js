4fee60ce97fe52f06bdaa5f82c27722926b1f46e
Yegor Trussov <egortr2004@mail.ru>
2020-07-28 19:41:26 +0600

    minesweeper promgram basic functionality completed

--- minesweeper/js/script.js
+++ minesweeper/js/script.js
@@ -1,6 +1,7 @@
 var board = document.querySelector('#board');
 var h = 20;
 var w = 20;
+var isPlaying = true;
 var grid = [];
 function init() {
     for (var i = 0; i < h; i++) {
@@ -13,17 +14,66 @@ function init() {
         grid.push(gridLine);
     }
     for (var i = 0; i < h; i++) {
-        var newLine = document.createElement('div');
         for (var j = 0; j < w; j++) {
+            var mineNum = 0;
+            if (i)
+                if (grid[i - 1][j].isMine)
+                    mineNum++;
+            if (j)
+                if (grid[i][j - 1].isMine)
+                    mineNum++;
+            if (i && j) {
+                if (grid[i - 1][j - 1].isMine)
+                    mineNum++;
+            }
+            if (i < w - 1 && j) {
+                if (grid[i + 1][j - 1].isMine)
+                    mineNum++;
+            }
+            if (i < h - 1)
+                if (grid[i + 1][j].isMine)
+                    mineNum++;
+            if (j < w - 1)
+                if (grid[i][j + 1].isMine)
+                    mineNum++;
+            if (i && j < h - 1) {
+                if (grid[i - 1][j + 1].isMine)
+                    mineNum++;
+            }
+            if (i < w - 1 && j < h - 1) {
+                if (grid[i + 1][j + 1].isMine)
+                    mineNum++;
+            }
+            grid[i][j].mineNum = mineNum;
+        }
+    }
+    for (var i = 0; i < h; i++) {
+        var newLine = document.createElement('div');
+        var _loop_1 = function (j) {
             var newNode = document.createElement('div');
             var currNode = grid[i][j];
             newNode.classList.add('node');
             if (currNode.isMine)
                 newNode.classList.add('mine');
             newNode.id = i + "-" + j;
+            newNode.addEventListener('click', function (e) {
+                if (currNode.isMine) {
+                    gameOver();
+                    return;
+                }
+                var target = e.target;
+                target.innerHTML = currNode.mineNum.toString();
+            });
             newLine.appendChild(newNode);
+        };
+        for (var j = 0; j < w; j++) {
+            _loop_1(j);
         }
         board.appendChild(newLine);
     }
+}
+function gameOver() {
+    isPlaying = false;
+    alert('Game over!');
 }
 init();
--- minesweeper/js/script.ts
+++ minesweeper/js/script.ts
@@ -2,6 +2,7 @@ let board: HTMLDivElement = document.querySelector('#board');
 
 let h: number = 20;
 let w: number = 20;
+let isPlaying: boolean = true;
 
 interface Node {
     isMine: boolean,
@@ -28,6 +29,33 @@ function init(): void {
     }
 
     for (let i: number = 0; i < h; i++) {
+        for (let j: number = 0; j < w; j++) {   
+            let mineNum: number = 0;
+            if (i) 
+                if (grid[i - 1][j].isMine) mineNum++;
+            if (j) 
+                if (grid[i][j - 1].isMine) mineNum++;
+            if (i && j) {
+                if (grid[i - 1][j - 1].isMine) mineNum++;
+            }
+            if (i < w - 1 && j) {
+                if (grid[i + 1][j - 1].isMine) mineNum++;
+            }
+            if (i < h - 1) 
+                if (grid[i + 1][j].isMine) mineNum++;
+            if (j < w - 1) 
+                if (grid[i][j + 1].isMine) mineNum++;
+            if (i && j < h - 1) {
+                if (grid[i - 1][j + 1].isMine) mineNum++;
+            }
+            if (i < w - 1 && j < h - 1) {
+                if (grid[i + 1][j + 1].isMine) mineNum++;
+            }
+            grid[i][j].mineNum = mineNum;
+        }
+    }
+
+    for (let i: number = 0; i < h; i++) {
         let newLine: HTMLDivElement = document.createElement('div');
         for (let j: number = 0; j < w; j++) {
             let newNode: HTMLDivElement = document.createElement('div');
@@ -36,10 +64,26 @@ function init(): void {
             if (currNode.isMine) 
                 newNode.classList.add('mine');
             newNode.id = `${ i }-${ j }`;
+            
+            newNode.addEventListener('click', function(e: Event): void {
+                if (currNode.isMine) {
+                    gameOver();
+                    return;
+                }
+
+                let target: HTMLDivElement = <HTMLDivElement>e.target;
+                target.innerHTML = currNode.mineNum.toString();
+            })
+
             newLine.appendChild(newNode);
         }
         board.appendChild(newLine)
     }
 }
+
+function gameOver(): void {
+    isPlaying = false;
+    alert('Game over!')
+} 
 
 init();
